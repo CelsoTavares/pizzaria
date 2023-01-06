97386e26a81497386add5ccad137336fb7f9651b
CelsoTavares <antoniocelsojr@yahoo.com.br>
2023-01-06 10:18:56 -0300

    :sparkles: carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -3,26 +3,37 @@ import { useEffect, useState } from "react";
 import pizza1 from "../../img/p3.jpg";
 import pizza2 from "../../img/p4.jpg";
 import pizza3 from "../../img/p5.jpg";
-
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 const Carousel = () => {
   const pizzas = [pizza1, pizza2, pizza3];
-  const [image, setImage] = useState(pizzas[0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const image = pizzas[currentIndex];
+
+  let intervalId: NodeJS.Timeout;
+  const lastIndex = pizzas.length - 1;
+
+  const prevSlide = () => {
+    setCurrentIndex(currentIndex === 0 ? lastIndex : currentIndex - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex(currentIndex === lastIndex ? 0 : currentIndex + 1);
+  };
 
   useEffect(() => {
-    setTimeout(() => {
-      if (image == pizzas[0]) {
-        setImage(pizzas[1]);
-      } else if (image == pizzas[1]) {
-        setImage(pizzas[2]);
-      } else {
-        setImage(pizzas[0]);
-      }
-    }, 4000);
-  }, [image]);
+    intervalId = setInterval(nextSlide, 3000);
+    return () => clearInterval(intervalId);
+  }, [nextSlide]);
 
   return (
     <C.Container>
-      <img src={image} alt="Mais pedidas" />;
+      <img src={image} alt="Mais pedidas" />
+      <button className="prev" onClick={prevSlide}>
+        <AiOutlineArrowLeft />
+      </button>
+      <button className="next" onClick={nextSlide}>
+        <AiOutlineArrowRight />
+      </button>
     </C.Container>
   );
 };
--- src/components/Carousel/styles.ts
+++ src/components/Carousel/styles.ts
@@ -4,9 +4,27 @@ export const Container = styled.div`
   height: 500px;
   overflow: hidden;
   box-shadow: 1px -1px 15px #000;
+  position: relative;
   img {
     width: 100%;
-    height: 500px;
+    height: 100%;
     transition: all 0.4s;
+  }
+  svg {
+    color: #fff;
+    border-radius: 20px;
+    background-color: #000;
+    font-size: 30px;
+    position: absolute;
+    cursor: pointer;
+    z-index: 10;
+  }
+  .prev svg {
+    bottom: 50%;
+    left: 5px;
+  }
+  .next svg {
+    bottom: 50%;
+    right: 5px;
   }
 `;
